318223e42fc3de401c4fe034ca9d4cbb5ddada85
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-06-19 23:45:19 +0300

    Implement GET

--- src/main/java/com/nmuzychuk/BookServlet.java
+++ src/main/java/com/nmuzychuk/BookServlet.java
@@ -1,17 +1,54 @@
 package com.nmuzychuk;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
+@WebServlet(urlPatterns = {"/books"}, asyncSupported = true)
 public class BookServlet extends HttpServlet {
+    static private Map<Integer, String> books;
+
+    @Override
+    public void init(ServletConfig servletConfig) {
+        books = new HashMap<>();
+
+        for (int i = 1; i <= 5; i++) {
+            books.put(i, "Book" + i);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("showing book");
+        req.startAsync();
+
+        final String paramId = req.getParameter("id");
+        final Writer out = resp.getWriter();
+
+        final AsyncContext asyncContext = req.getAsyncContext();
+        asyncContext.start(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (paramId == null) {
+                        out.write(books.toString());
+                    } else {
+                        int id = Integer.parseInt(paramId);
+                        out.write(books.get(id));
+                    }
+                    asyncContext.complete();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
